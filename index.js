52f511c4b4c39b0899f911b80ffb6a00d1d94c28
humphrey <writernic89@gmail.com>
2024-03-31 01:02:03 +0300

    dom file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the element where you want to change the text
+    const textElement = document.getElementById("text");
+
+    // Change the text content to "This is really cool!"
+    textElement.textContent = "This is really cool!";
+});
+
